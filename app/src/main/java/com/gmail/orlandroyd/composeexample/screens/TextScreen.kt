effe3c5bf4051aea66bfadf8c1bb91853e488e4b
orlando.ppena <orlando.ppena@reduc.edu.cu>
2021-10-13 18:25:32 -0400

    TextScreen

--- app/src/main/java/com/gmail/orlandroyd/composeexample/screens/TextScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/composeexample/screens/TextScreen.kt
@@ -36,9 +36,17 @@ package com.gmail.orlandroyd.composeexample.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.gmail.orlandroyd.composeexample.R
 import com.gmail.orlandroyd.composeexample.router.BackButtonHandler
 import com.gmail.orlandroyd.composeexample.router.JetFundamentalsRouter
 import com.gmail.orlandroyd.composeexample.router.Screen
@@ -58,7 +66,14 @@ fun TextScreen() {
     }
 }
 
+@Preview
 @Composable
 fun MyText() {
-    //TODO add your code here
+    Text(
+        text = stringResource(id = R.string.jetpack_compose),
+        fontStyle = FontStyle.Italic,
+        color = colorResource(R.color.colorPrimary),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
