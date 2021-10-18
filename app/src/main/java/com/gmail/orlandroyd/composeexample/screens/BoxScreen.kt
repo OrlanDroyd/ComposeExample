7cdb75cff3d69eaab522868b74d3ffb36a2583d8
orlando.ppena <orlando.ppena@reduc.edu.cu>
2021-10-18 17:25:30 -0400

    BoxScreen

--- app/src/main/java/com/gmail/orlandroyd/composeexample/screens/BoxScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/composeexample/screens/BoxScreen.kt
@@ -35,8 +35,16 @@
 
 package com.gmail.orlandroyd.composeexample.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.gmail.orlandroyd.composeexample.R
 import com.gmail.orlandroyd.composeexample.router.BackButtonHandler
 import com.gmail.orlandroyd.composeexample.router.JetFundamentalsRouter
 import com.gmail.orlandroyd.composeexample.router.Screen
@@ -51,6 +59,33 @@ fun BoxScreen() {
 }
 
 @Composable
-fun MyBox(modifier: Modifier = Modifier) {
-    //TODO write your code here
+fun MyBox(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = R.string.first),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.TopStart)
+        )
+        Text(
+            text = stringResource(id = R.string.second),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.Center)
+        )
+        Text(
+            text = stringResource(id = R.string.third),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.BottomEnd)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun MyBoxPrev(){
+    MyBox()
 }
