7f33ed3c9debc6ea1fb078f2089264f56cfa7548
orlando.ppena <orlando.ppena@reduc.edu.cu>
2021-10-18 17:17:02 -0400

    ColumnScreen

--- app/src/main/java/com/gmail/orlandroyd/composeexample/screens/ColumnScreen.kt
+++ app/src/main/java/com/gmail/orlandroyd/composeexample/screens/ColumnScreen.kt
@@ -35,7 +35,16 @@
 
 package com.gmail.orlandroyd.composeexample.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.gmail.orlandroyd.composeexample.router.BackButtonHandler
 import com.gmail.orlandroyd.composeexample.router.JetFundamentalsRouter
 import com.gmail.orlandroyd.composeexample.router.Screen
@@ -50,6 +59,18 @@ fun ColumnScreen() {
 }
 
 @Composable
+@Preview
 fun MyColumn() {
-    //TODO write your code here
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        THREE_ELEMENT_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 22.sp
+            )
+        }
+    }
 }
